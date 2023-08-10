c7e3bda6eabe78d919f380fb15f4856cb2f4e880
shivam kumar <20051252@kiit.ac.in>
2023-08-10 21:54:42 +0530

    Client looks good

--- src/components/Login.js
+++ src/components/Login.js
@@ -129,17 +129,6 @@ function Login() {
                 <ToastContainer />
                 {
                     shivamMsg && 
-                    toast.error("!!! IMPORTANT !!!", {
-                        position: "top-center",
-                        autoClose: 10000,
-                        hideProgressBar: true,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                        })
-                    &&
                     toast.info("In Security Token field Either Generate a Token or Copy Paste Someone's Generated Token ONLY.", {
                         position: "top-center",
                         autoClose: 60000,
