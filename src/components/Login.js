5a74d9c8bca80c1b98d77820c73ba7828ad5147c
shivam kumar <20051252@kiit.ac.in>
2023-08-10 13:49:17 +0530

    code condensed successfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import io from "socket.io-client";
 import { FaCopy } from 'react-icons/fa';
